b6c7e0d586387858f744df86adc5670df956b066
Hong Bunleab <bunleabfb@gmail.com>
2025-10-26 08:04:17 +0700

    I created contact form

---
+++ contact.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact</title>
+</head>
+<body>
+    <form action="">
+        Username: <input type="text">
+        Email: <input type="email">
+        <input type="submit" value="Send">
+    </form>
+</body>
+</html>
